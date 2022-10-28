fd596ad882cff58b4c2b61497bc9df2c22d50616
Daniel Bazaco <danielbazacovelasco@gmail.com>
2022-10-28 20:18:29 +0200

    Added wildcard transition

--- test/autocompleter.test.js
+++ test/autocompleter.test.js
@@ -1,10 +1,28 @@
 
-import {givenGrammar} from './utils/GrammarTest';
+import {givenGrammar, givenLexer} from './utils/GrammarTest';
 
-test('Test Grammar', async () => {
-    await givenGrammar("r: A A 'B'; A: 'A';").whenInput("AA").thenExpect("B");
-    await givenGrammar("r: 'A' ('B'|'C'|'D') EOF;").whenInput("A").thenExpect(["B","C","D"]);
-    await givenGrammar("r: 'A' EOF;").whenInput("A").thenExpect("EOF");
-    await givenGrammar("r: 'A';").whenInput("A").thenExpect([]);
-    await givenGrammar("r: 'A' 'B'? 'C' EOF;").whenInput("A").thenExpect(["B","C"]);
+describe('Test Autocompletition', () => {
+    const LETTER_LEXER = "A: 'A'; B: 'B'; C: 'C'";
+
+    it('General', async () => {
+      await givenGrammar("r: A A 'B'; A: 'A';").whenInput("AA").thenExpect("B");
+      await givenGrammar("r: 'A' ('B'|'C'|'D') EOF;").whenInput("A").thenExpect(["B","C","D"]); //TODO test also with optionals, and optional rules
+      await givenGrammar("r: 'A' EOF;").whenInput("A").thenExpect("EOF");
+      await givenGrammar("r: 'A' 'B'? 'C' EOF;").whenInput("A").thenExpect(["B","C"]);
+      await givenGrammar("r: A+ B; A: 'A'; B:'B';").whenInput("A").thenExpect(["A", "B"]);
+    });
+
+    //TODO test negated sets and negated optional sets
+    it('Inline tokens return their value', async () => {
+      await givenGrammar("r: 'A';").whenInput("A").thenExpect([]);
+    });
+    //TODO test tokens in other channels
+    it('Test dots', async () => {
+      await givenGrammar("r: .; A:'a'; B: 'b';").whenInput("").thenExpect(["A", "B"]);
+    });
+
+    it('Ignores tokens in other channels', async () => {
+      await givenLexer("channels {POTATO}\n A:'a' -> channel(HIDDEN); B: 'b' -> channel(POTATO); C:'c';").andParser("r: .;").whenInput("").thenExpect(["C"])
+    });
+
 });
--- test/utils/GrammarTest.js
+++ test/utils/GrammarTest.js
@@ -1,10 +1,6 @@
 import Autocompleter from '../../lib/autocompleter';
 import child from 'child_process';
 import fs from 'fs';
-import path from 'path';
-
-console.log(path.resolve("./"))
-
 
 if (!fs.existsSync(`./test/tmp/`)) {
     fs.mkdirSync(`./test/tmp/`);
@@ -40,7 +36,51 @@ class GrammarTest {
     }
 }
 
+//TODO would be nice to combine it with GrammarTest 
+class SplitGrammarTest {
+    constructor(lexer) {
+        this._lexer = lexer;
+        this._input = null;
+        this.counter = counter++;
+    }
+
+    get fullLexer() {
+        return  `lexer grammar file${this.counter}Lexer;\n` + this._lexer;
+    }
+
+    get fullParser() {
+        return  `parser grammar file${this.counter}Parser;\n options { tokenVocab=file${this.counter}Lexer;}` + this._parser;
+    }
+
+    andParser(parser) {
+        this._parser = parser;
+        return this;
+    }
+
+    whenInput(input) {
+        this.input = input;
+        return this;
+    }
+
+    async thenExpect(expected) {
+        if (!Array.isArray(expected)) expected = [expected];
+        const file = `file${this.counter}`
+        fs.writeFileSync(`./test/tmp/${file}Lexer.g4`, this.fullLexer);
+        fs.writeFileSync(`./test/tmp/${file}Parser.g4`, this.fullParser);
+        child.execSync(`java -jar ./test/bin/antlr-4.11.1-complete.jar -Dlanguage=JavaScript ./test/tmp/${file}Lexer.g4  -no-visitor -no-listener -o ./test/tmp/`)
+        child.execSync(`java -jar ./test/bin/antlr-4.11.1-complete.jar -Dlanguage=JavaScript ./test/tmp/${file}Parser.g4  -no-visitor -no-listener -o ./test/tmp/`)
+        const Lexer = await import(`../tmp/${file}Lexer.js`)
+        const Parser = await import(`../tmp/${file}Parser.js`)
+        const ac = new Autocompleter(Lexer.default, Parser.default);
+        const result = ac.autocomplete(this.input);
+        expect(result).toEqual(expected);
+    }
+}
+
 export function givenGrammar(grammar) {
     return new GrammarTest(grammar);
 }
 
+export function givenLexer(lexer) {
+    return new SplitGrammarTest(lexer);
+}
